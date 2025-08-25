524a71ce8f237ae30d2ef726652225376131ac2a
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-08-25 10:13:19 +0300

    practice complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,31 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  // 1. Handle filter change
+  function onChangeType(type) {
+    setFilters({ ...filters, type });
+  }
+
+  // 2. Handle fetch based on filter
+  function onFindPetsClick() {
+    let url = "http://localhost:3001/pets";
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+
+    fetch(url)
+      .then((r) => r.json())
+      .then((data) => setPets(data));
+  }
+
+  // 3. Handle adoption
+  function onAdoptPet(id) {
+    const updatedPets = pets.map((p) =>
+      p.id === id ? { ...p, isAdopted: true } : p
+    );
+    setPets(updatedPets);
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +40,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={onChangeType}
+              onFindPetsClick={onFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
